1d23d1c465e3eb299cc3911e9a34cb0929ead9e6
Lazzytchik <lazzytchik008@gmail.com>
2022-09-07 00:26:38 +0300

    Composer and Autoload added, Handler class skeleton established.

---
+++ classes/TextHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Arse\PhpCommandTest;
+
+class TextHandler {
+
+    public const COMMA = ',';
+    public const SEMICOLON = ';';
+
+    public static function execute($args){
+        return $args;
+    }
+
+    public static function countAverageLineCount()
+    {
+
+    }
+
+    public static function replaceDates()
+    {
+
+    }
+
+}
